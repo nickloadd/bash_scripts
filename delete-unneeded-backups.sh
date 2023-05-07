fa60fe42aa40b9bb466331daf21870c3228b6060
nickloadd <meraxpan@mail.ru>
2023-05-07 23:30:24 +0200

    Added delete undeeded backups sh

---
+++ delete-unneeded-backups.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+year = $(date +%Y)
+previous_month = $(date -d "`date +%Y-%m-01` -1day" +'%m')
+while [ ! -z "$(clickhouse-backup list remote | grep "$year-$previous_month-[0-3][0-9]-daily")" ]
+do
+  remove_backup=$(clickhouse-backup list remote | grep "$year-$previous_month-[0-3][0-9]-daily"| head -n 1 | cut -d " " -f 1)
+  clickhouse-backup delete remote $remove_backup
+  echo "$remove_backup was deleted from S3 storage"
+done
