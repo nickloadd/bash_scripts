0af78beb28b50dcb7be49e52a94a5349207a1b75
nickloadd <meraxpan@mail.ru>
2023-05-07 20:21:31 +0200

    Updated click-backup sh

--- click-backup-cron.sh
+++ click-backup-cron.sh
@@ -1,18 +1,29 @@
+#Preparation:
+#Create click DB user with access to target database or tables and select on system.*
+#Install clickhouse-backup tool : https://github.com/AlexAkulov/clickhouse-backup
+#Create clickhouse-backup config with "backups_to_keep_local: 0" settings. For example, /etc/clickhouse-backup/config-monthly.yml
+
+#Body of script:
 #!/bin/bash
 set +x
 period=$1
 
 backup_name=$(date +%Y-%m-%d)
-day=$(date +%d)
 
 echo "Creating local backup '${backup_name}'"
 clickhouse-backup create "${backup_name}"
 
 if [[ "daily" == "${period}" && "2" -le "$(clickhouse-backup list local | wc -l)" ]]; then
   prev_backup_name="$(clickhouse-backup list local | tail -n 2 | head -n 1 | cut -d " " -f 1)"
-  echo "Uploading the backup '${backup_name}' as diff from the previous backup ('${prev_backup_name}')"
-  clickhouse-backup upload --diff-from "${prev_backup_name}" "${backup_name}-diff"
+  echo "Uploading the backup '${backup_name}-${period}' as diff from the previous backup ('${prev_backup_name}')"
+  clickhouse-backup upload --diff-from "${prev_backup_name}" "${backup_name}-${period}"
 elif [[ "monthly" == "${period}" ]]; then
-  echo "Uploading the backup '${backup_name}, and removing old unneeded backups"
-  KEEP_BACKUPS_LOCAL=1 KEEP_BACKUPS_REMOTE=1 clickhouse-backup upload "${backup_name}-full"
+  echo "Uploading the backup '${backup_name}-${period}, and removing old unneeded backups"
+  clickhouse-backup upload "${backup_name}-${period}" -c /etc/clickhouse-backup/config-monthly.yml
 fi
+
+#Cron tabs:
+#Monthly job to make full backup
+0 0 1 * * /opt/click-backup.sh monthly
+#Daily job to make diff backup
+0 0 2-31 * * /opt/click-backup.sh daily
