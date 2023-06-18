04bd31a231f389f5c010f84dc233422e5eb9d05b
nickloadd <meraxpan@mail.ru>
2023-06-18 20:08:42 +0200

    Added down l2tp/ipsec script

---
+++ l2tp+ipsec/down_tunnel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PPP_GW_ADD=`./getip.sh ppp0` # get peer ip 
+ip route del 192.168.1.3 via $PPP_GW_ADD dev ppp0 # drop route
+
+# down connection
+ipsec auto --down L2TP-PSK 
+echo "d vpn-connection" > /var/run/xl2tpd/l2tp-control
+systemctl stop xl2tpd
+systemctl stop ipsec
